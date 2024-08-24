bd0d252bc94daac047681a8b8e3d7fd03aa4b952
kai <nacci0829@gmail.com>
2024-08-25 02:09:42 +0900

    keyPadの編集

--- src/features/game/components/KeyPad.tsx
+++ src/features/game/components/KeyPad.tsx
@@ -5,7 +5,7 @@ type KeyPadProps = {
 };
 
 const KeyPad: React.FC<KeyPadProps> = ({ onKeyPress }) => {
-  const keys = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+  const keys = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
   const handleKeyPress = (key: string) => {
     onKeyPress(key);
@@ -17,11 +17,20 @@ const KeyPad: React.FC<KeyPadProps> = ({ onKeyPress }) => {
         <button
           key={key}
           onClick={() => handleKeyPress(key)}
-          className="w-20 h-20 text-2xl font-bold bg-white border rounded-lg shadow-md hover:bg-gray-200 active:bg-black active:text-white"
+          className="w-[108px] h-14 text-2xl font-bold bg-white border rounded-lg shadow-md active:bg-black active:text-white"
         >
           {key}
         </button>
       ))}
+      <div className="col-span-3 flex justify-center">
+        <button
+          key={"0"}
+          onClick={() => handleKeyPress("0")}
+          className="w-[108px] h-14 text-2xl font-bold bg-white border rounded-lg shadow-md active:bg-black active:text-white"
+        >
+          0
+        </button>
+      </div>
     </div>
   );
 };
