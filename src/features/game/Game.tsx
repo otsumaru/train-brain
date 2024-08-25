3b318861cf287dd67b68eacb9c69be2c39bf3805
kai <nacci0829@gmail.com>
2024-08-25 18:48:00 +0900

    時間の処理を途中まで

--- src/features/game/Game.tsx
+++ src/features/game/Game.tsx
@@ -1,26 +1,76 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import KeyPad from "./components/KeyPad";
 
 const Game = () => {
   const [input, setInput] = useState("");
+  const [time, setTime] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+  const [countdown, setCountdown] = useState(3);
 
   const handleKeyPress = (key: string) => {
-    setInput((prev) => prev + key);
+    if (time > 0) {
+      setInput((prev) => prev + key);
+    }
+  };
+
+  useEffect(() => {
+    let counter = 3;
+    setCountdown(counter);
+    const interval = setInterval(() => {
+      counter--;
+      setCountdown(counter);
+      if (counter === 0) {
+        clearInterval(interval);
+        setCountdown(0);
+        setIsActive(true);
+      }
+    }, 1000);
+  }, []);
+
+  useEffect(
+    () => {
+      const interval = setInterval(() => {
+        if (isActive) {
+          setTime((prevTime) => prevTime + 1);
+        } else if (!isActive && time !== 0) {
+          clearInterval(interval);
+        }
+        return () => clearInterval(interval);
+      }, 10);
+    }, // 10ミリ秒ごとに更新 (0.01秒)
+
+    [isActive, time]
+  );
+
+  const formatTime = (time: number) => {
+    const seconds = Math.floor((time % 6000) / 100);
+    const milliseconds = time % 100;
+
+    return `${seconds < 10 ? `0${seconds}` : seconds}:${
+      milliseconds < 10 ? `0${milliseconds}` : milliseconds
+    }`;
   };
 
   return (
-    <div className="pt-20 px-4">
-      <p className="mt-2 text-center text-3xl">2:03</p>
-      <p className=" text-xl font-bold text-gray-700">第２問</p>
+    <div className="relative pt-20 px-4">
+      {countdown > 0 && (
+        <div className="absolute rounded-xl z-10 mx-auto w-11/12 h-64 flex justify-center items-center text-8xl font-semibold bg-white">
+          <p>{countdown}</p>
+        </div>
+      )}
+      <div className="mt-2 text-center text-3xl">
+        <span>2:03</span>
+      </div>
+      <p className=" text-lg] font-bold text-gray-700">第２問</p>
       <div className="h-14 mx-auto w-60 rounded-lg flex justify-center items-center bg-gray-200 text-lg">
         <span>3×4=</span>
         <span></span>
       </div>
-      <p className="text-xl font-bold text-gray-700">第１問</p>
+      <p className="text-xl font-bold text-black">第１問</p>
       <div className="h-28 rounded-xl flex justify-center items-center bg-white font-bold text-6xl shadow-lg">
-        <span>3+5=</span>
-        <span>8</span>
+        <span id="question">3+5=</span>
+        <span id="answer">{input}</span>
       </div>
       <KeyPad onKeyPress={handleKeyPress} />
     </div>
@@ -28,3 +78,12 @@ const Game = () => {
 };
 
 export default Game;
+
+{
+  /* <input
+  onChange={() => handleKeyPress}
+  type="text"
+  className="w-20 h-20"
+  value={input}
+/>; */
+}
