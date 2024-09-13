413cd6256336734eec2a8bd7d3633ff242410f78
kai <nacci0829@gmail.com>
2024-09-13 13:55:01 +0900

    google search console用の記述を追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -90,6 +90,10 @@ export default function RootLayout({
         <meta name="twitter:image:width" content="287" />
         <meta name="twitter:image:height" content="356" />
         <meta name="twitter:image:alt" content="秒速計算ノックのイメージ" />
+        <meta
+          name="google-site-verification"
+          content="SqGfzy3Mmgi75iPmU7FD1X96b1b1W9tAAhhQS_RYMKQ"
+        />
       </Head>
       <body className={inter.className}>{children}</body>
     </html>
